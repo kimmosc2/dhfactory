14708e48602e109e6c3c8d508bab4f1dce649e06
ZhiSonghao <55502609@qq.com>
2019-02-04 23:48:27 +0800

    add factory.html and error page

--- assets/js/factorylist.js
+++ assets/js/factorylist.js
@@ -1,22 +1,19 @@
+
+
 var flist = new Vue({
     el: '#factory-body',
     data: {
         factorys: [
-            { "factoryname": "上海日腾", "salary": "3500-4500",   "img": "../assets/images/factory/上海日腾.jpg",  "remarks": "不限男女" },
-            { "factoryname": "苏州华硕", "salary": "3800-4500",   "img": "../assets/images/factory/苏州华硕.jpg",  "remarks": "不限男女" },
-            { "factoryname": "苏州佳世达", "salary": "3500-4500",   "img": "../assets/images/factory/苏州佳世达.jpg",  "remarks": "不限男女" },
-            { "factoryname": "上海昌硕", "salary": "3800-5500",   "img": "../assets/images/factory/上海昌硕.jpg",  "remarks": "不限男女" },
-            { "factoryname": "昆山世硕", "salary": "4000-5200",   "img": "../assets/images/factory/昆山世硕.jpg",  "remarks": "不限男女" },
-            { "factoryname": "上海英华达", "salary": "4000-4500",   "img": "../assets/images/factory/上海英华达.jpg",  "remarks": "不限男女" },
-            { "factoryname": "深圳富士康", "salary": "5000-6500",   "img": "../assets/images/factory/深圳富士康.jpg",  "remarks": "不限男女" },
-            { "factoryname": "无锡优科多", "salary": "3500-4500",   "img": "../assets/images/factory/无锡优科多.jpg",  "remarks": "不限男女" },
-            { "factoryname": "上海日铭", "salary": "4000-4500",   "img": "../assets/images/factory/上海日铭.jpg",  "remarks": "不限男女" },
-            { "factoryname": "常熟新世电子", "salary": "4000-4500",   "img": "../assets/images/factory/常熟新世电子.jpg",  "remarks": "不限男女" },
-            
-        ],
-        img: [
-            "123", "321", "12345"
+            { "factoryname": "上海日腾", "salary": "3500-4500", "img": "../assets/images/factory/上海日腾.jpg", "remarks": "不限男女" },
+            { "factoryname": "苏州华硕", "salary": "3800-4500", "img": "../assets/images/factory/苏州华硕.jpg", "remarks": "不限男女" },
+            { "factoryname": "苏州佳世达", "salary": "3500-4500", "img": "../assets/images/factory/苏州佳世达.jpg", "remarks": "不限男女" },
+            { "factoryname": "上海昌硕", "salary": "3800-5500", "img": "../assets/images/factory/上海昌硕.jpg", "remarks": "不限男女" },
+            { "factoryname": "昆山世硕", "salary": "4000-5200", "img": "../assets/images/factory/昆山世硕.jpg", "remarks": "不限男女" },
+            { "factoryname": "上海英华达", "salary": "4000-4500", "img": "../assets/images/factory/上海英华达.jpg", "remarks": "不限男女" },
+            { "factoryname": "深圳富士康", "salary": "5000-6500", "img": "../assets/images/factory/深圳富士康.jpg", "remarks": "不限男女" },
+            { "factoryname": "无锡优科多", "salary": "3500-4500", "img": "../assets/images/factory/无锡优科多.jpg", "remarks": "不限男女" },
+            { "factoryname": "上海日铭", "salary": "4000-4500", "img": "../assets/images/factory/上海日铭.jpg", "remarks": "不限男女" },
+            { "factoryname": "常熟新世电子", "salary": "4000-4500", "img": "../assets/images/factory/常熟新世电子.jpg", "remarks": "不限男女" }
         ]
-
     }
 })
